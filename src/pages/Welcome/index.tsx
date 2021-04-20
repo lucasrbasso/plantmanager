a99d9af07a0d05a9e517b2622e709f2e515fcf61
Lucas Basso <lucas.rbasso@outlook.com>
2021-04-20 19:26:31 -0300

    pages: welcome done

--- src/pages/Welcome/index.tsx
+++ src/pages/Welcome/index.tsx
@@ -1,26 +1,43 @@
-import React from 'react';
+import { useNavigation } from '@react-navigation/native';
+import React, { useCallback } from 'react';
 
 import wateringImg from '../../assets/watering.png';
-import Button from '../../components/Button';
-import { Container, Title, Image, Description } from './styles';
+import {
+    Container,
+    Title,
+    Image,
+    Description,
+    Button,
+    ButtonIcon,
+} from './styles';
 
-const Welcome: React.FC = () => (
-    <Container>
-        <Title>
-            Gerencie
-            {'\n'}
-            suas plantas de
-            {'\n'}
-            forma fácil
-        </Title>
-        <Image source={wateringImg} />
-        <Description>
-            Não esqueça mais de regar suas plantas. Nós cuidamos de lembrar você
-            sempre que precisar.
-        </Description>
+const Welcome: React.FC = () => {
+    const { navigate } = useNavigation();
 
-        <Button activeOpacity={0.7}>Avançar</Button>
-    </Container>
-);
+    const handleStart = useCallback(() => {
+        navigate('UserIdentification');
+    }, [navigate]);
+
+    return (
+        <Container>
+            <Title>
+                Gerencie
+                {'\n'}
+                nossas plantas de
+                {'\n'}
+                forma fácil
+            </Title>
+            <Image source={wateringImg} resizeMode="contain" />
+            <Description>
+                Não esqueça mais de regar suas plantas. Nós cuidamos de lembrar
+                você sempre que precisar.
+            </Description>
+
+            <Button onPress={handleStart} activeOpacity={0.7}>
+                <ButtonIcon name="chevron-right" />
+            </Button>
+        </Container>
+    );
+};
 
 export default Welcome;
--- src/pages/Welcome/styles.ts
+++ src/pages/Welcome/styles.ts
@@ -1,28 +1,48 @@
+import { RectButton } from 'react-native-gesture-handler';
+import { Dimensions } from 'react-native';
+import { Entypo } from '@expo/vector-icons';
 import styled from 'styled-components/native';
 import colors from '../../styles/colors';
+import fonts from '../../styles/fonts';
 
-export const Container = styled.SafeAreaView`
+export const Container = styled.View`
     flex: 1;
     align-items: center;
-    justify-content: space-between;
+    justify-content: space-around;
+    padding: 0 30px;
 `;
 
 export const Title = styled.Text`
+    font-family: ${fonts.heading};
     font-size: 32px;
-    font-weight: bold;
+    line-height: 38px;
     text-align: center;
     color: ${colors.heading};
     margin-top: 38px;
 `;
 
 export const Image = styled.Image`
-    width: 292px;
-    height: 300px;
+    height: ${Dimensions.get('window').width * 0.7}px;
 `;
 
 export const Description = styled.Text`
     text-align: center;
+    font-family: ${fonts.text};
     font-size: 18px;
-    padding: 0 30px;
     color: ${colors.heading};
+`;
+
+export const Button = styled(RectButton)`
+    background-color: ${colors.green};
+    justify-content: center;
+    align-items: center;
+    border-radius: 16px;
+    margin-bottom: 30px;
+    height: 56px;
+    width: 56px;
+`;
+
+export const ButtonIcon = styled(Entypo)`
+    color: ${colors.white};
+    font-size: 28px;
 `;
