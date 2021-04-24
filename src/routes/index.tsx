310c78825e29cfe9d3adb0e54a70a589f0672bef
Lucas Basso <lucas.rbasso@outlook.com>
2021-04-23 21:24:06 -0300

    routes now working

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import AppLoading from 'expo-app-loading';
 
-import StackRoutes from './stack.routes';
+import AppRoutes from './stack.routes';
+import { useAuth } from '../hooks/Auth';
+import AuthRoutes from './auth.routes';
 
-const Routes: React.FC = () => (
-    <NavigationContainer>
-        <StackRoutes />
-    </NavigationContainer>
-);
+const Routes: React.FC = () => {
+    const { user, loading } = useAuth();
+
+    if (loading) {
+        return <AppLoading />;
+    }
+
+    return user ? (
+        <NavigationContainer>
+            <AppRoutes />
+        </NavigationContainer>
+    ) : (
+        <NavigationContainer>
+            <AuthRoutes />
+        </NavigationContainer>
+    );
+};
 
 export default Routes;
