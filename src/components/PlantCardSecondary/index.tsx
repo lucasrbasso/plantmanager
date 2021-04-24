17b36ea8b5b47223e6d170ae07fef7bd57e3e7f9
Lucas Basso <lucas.rbasso@outlook.com>
2021-04-23 21:24:26 -0300

    delete button added

--- src/components/PlantCardSecondary/index.tsx
+++ src/components/PlantCardSecondary/index.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import { SvgFromUri } from 'react-native-svg';
 import { RectButtonProperties } from 'react-native-gesture-handler';
+import Swipeable from 'react-native-gesture-handler/Swipeable';
 
+import Animated from 'react-native-reanimated';
+import { Feather } from '@expo/vector-icons';
+import { View } from 'react-native';
 import {
     Container,
+    DeleteButton,
     ButtonTitle,
     PlantDetails,
     PlantText,
     PlantTime,
 } from './styles';
+import colorTheme from '../../styles/colors';
+
+const { colors } = colorTheme();
 
 interface PlantProps extends RectButtonProperties {
     data: {
@@ -16,18 +24,40 @@ interface PlantProps extends RectButtonProperties {
         photo: string;
         hour: string;
     };
+    handleRemove: () => void;
 }
 
-const PlantCardSecondary: React.FC<PlantProps> = ({ data, ...rest }) => {
+const PlantCardSecondary: React.FC<PlantProps> = ({
+    data,
+    handleRemove,
+    ...rest
+}) => {
     return (
-        <Container {...rest}>
-            <SvgFromUri uri={data.photo} width={60} height={60} />
-            <ButtonTitle>{data.name}</ButtonTitle>
-            <PlantDetails>
-                <PlantText>Regar as</PlantText>
-                <PlantTime>{data.hour}</PlantTime>
-            </PlantDetails>
-        </Container>
+        <Swipeable
+            overshootRight={false}
+            renderRightActions={() => (
+                <Animated.View>
+                    <View style={{ flex: 1, alignItems: 'flex-end' }}>
+                        <DeleteButton onPress={handleRemove}>
+                            <Feather
+                                name="trash"
+                                size={32}
+                                color={colors.white}
+                            />
+                        </DeleteButton>
+                    </View>
+                </Animated.View>
+            )}
+        >
+            <Container {...rest}>
+                <SvgFromUri uri={data.photo} width={60} height={60} />
+                <ButtonTitle>{data.name}</ButtonTitle>
+                <PlantDetails>
+                    <PlantText>Regar as</PlantText>
+                    <PlantTime>{data.hour}</PlantTime>
+                </PlantDetails>
+            </Container>
+        </Swipeable>
     );
 };
 
